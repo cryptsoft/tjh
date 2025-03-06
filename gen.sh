d98916f7fd6316aaf23e2d67f2d612bb9071bcbb
Tim Hudson <tjh@cryptsoft.com>
2025-03-06 13:53:25 +1000

    another attempt to fix dark mode

--- gen.sh
+++ gen.sh
@@ -20,5 +20,5 @@ MMDC="mmdc"
 
 
 $SHOWDOWN -i ml-dsa-external-mu-edit.md -o ml-dsa-external-mu.html
-$MMDC -i ml-dsa-external-mu-edit.md -o ml-dsa-external-mu.md
+$MMDC -t dark -b transparent -i ml-dsa-external-mu-edit.md -o ml-dsa-external-mu.md
 
